050c8a206cd0597783fa13455fb23ac5224444ac
Tymon Szczepanowski <tymekszczepanowski@gmail.com>
2023-12-06 23:44:41 +0100

    Add network template

---
+++ dl_project/network.py
@@ -0,0 +1,29 @@
+#!/bin/env python3
+
+import torch
+from pytorch_lightning import LightningModule
+
+
+class BhmthNet:
+    def __init__(self, train, test):
+        pass
+
+    # Network layers
+    def forward(self, x):
+        pass
+
+    # 6 Sections from Ligthning Module
+    def training_step(self):
+        pass
+
+    def validation_step(self):
+        pass
+
+    def test_step(self):
+        pass
+
+    def predict_step(self):
+        pass
+
+    def configure_optimizers(self):
+        pass
--- main.py
+++ main.py
@@ -4,5 +4,9 @@ from torch.utils.data import DataLoader
 from dl_project.packetdataset import PacketDataset
 
 if __name__ == "__main__":
+    # Prepare data
     training_data = PacketDataset("datasets/UNSW_NB15_training.csv")
+    testing_data = PacketDataset("datasets/UNSW_NB15_testing.csv")
+
     train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+    test_dataloader = DataLoader(testing_data, batch_size=64, shuffle=True)

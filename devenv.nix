4efe1e5596ef9cea20e3711fd576894009c18ba6
Tymon Szczepanowski <tymon.szczepanowski@gmail.com>
2024-01-30 23:59:13 +0100

    Use poetry to pinpoint python packages, add support for gpu

--- devenv.nix
+++ devenv.nix
@@ -3,23 +3,27 @@
 {
   # https://devenv.sh/packages/
   packages = [
-    (pkgs.python311.withPackages (ps: [
-      ps.numpy
-      ps.pandas
-      ps.dateutil
-      ps.pytz
-      ps.torch
-      ps.matplotlib
-      ps.pytorch-lightning
-    ]))
+    pkgs.stdenv.cc.cc.lib
+    pkgs.cudaPackages.cudatoolkit
+    pkgs.linuxPackages.nvidia_x11
   ];
 
-  # https://devenv.sh/basics/
-  env.GREET = "dl-project-shell";
-  scripts.hello.exec = "echo hello from $GREET";
+  languages.python = {
+    enable = true;
+    package = pkgs.python311;
+    poetry.enable = true;
+    poetry.install.enable = true;
+  };
 
   enterShell = ''
-    hello
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    # export LD_LIBRARY_PATH=/usr/lib/wsl/lib:${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib:${pkgs.stdenv.cc.cc.lib}/lib # WSL CASE
+    export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+    export EXTRA_CCFLAGS="-I/usr/include"
+      echo ================================================
+      echo Nix devenv shell for Network Intrusion Detection
+      echo To add python modules use 'poetry add'
+      echo ================================================
   '';
 
   pre-commit.hooks.black.enable = true;
